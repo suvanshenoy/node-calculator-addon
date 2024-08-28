24c2cf80c16dd2cb57bcffb648ef54717ffc3332
suvanshenoy <suvanshenoy155@gmail.com>
2024-08-29 03:56:13 +0530

    Remove header

--- src/calculator/calculator.hpp
+++ src/calculator/calculator.hpp
@@ -2,14 +2,11 @@
 
 #include "utils.hpp"
 
-template <typename T> class Calculator
-{
-  public:
-    Calculator() = default;
-    T add(const T &a, const T &b) const;
-    T subtract(const T &a, const T &ob) const;
-    T multiply(const T &a, const T &b) const;
-    Optional<T> divide(const T &a, const T &b) const;
+template <typename T> class Calculator {
+public:
+  Calculator() = default;
+  T add(const T &a, const T &b) const;
+  T subtract(const T &a, const T &ob) const;
+  T multiply(const T &a, const T &b) const;
+  Optional<T> divide(const T &a, const T &b) const;
 };
-
-#include "calculator.tpp"
