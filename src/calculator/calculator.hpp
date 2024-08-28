a14377032a8ab9508c8cf71c5c8ac84addc7d123
suvanshenoy <suvanshenoy155@gmail.com>
2024-08-29 04:00:14 +0530

    Prefix each method with 'constexpr inline'

--- src/calculator/calculator.hpp
+++ src/calculator/calculator.hpp
@@ -5,8 +5,8 @@
 template <typename T> class Calculator {
 public:
   Calculator() = default;
-  T add(const T &a, const T &b) const;
-  T subtract(const T &a, const T &ob) const;
-  T multiply(const T &a, const T &b) const;
-  Optional<T> divide(const T &a, const T &b) const;
+  constexpr inline T add(const T &a, const T &b) const;
+  constexpr inline T subtract(const T &a, const T &ob) const;
+  constexpr inline T multiply(const T &a, const T &b) const;
+  constexpr inline Optional<T> divide(const T &a, const T &b) const;
 };
